ce6a1d0312658fca5103db7991c9b269e77f90db
seoyoon <seoyoon130@kakao.com>
2022-08-12 15:34:13 +0900

    temp: seoyoon branch refactoring

---
+++ src/main/java/com/example/demo/src/Comment/CommentDao.java
@@ -0,0 +1,32 @@
+package com.example.demo.src.Comment;
+
+import com.example.demo.src.Comment.model.PostCommentReq;
+import com.example.demo.src.Comment.model.PostCommentRes;
+import com.example.demo.src.User.model.GetProfileRes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Repository
+public class CommentDao {
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public PostCommentRes createComment(int pageId,int blockId, PostCommentReq commentReq) {
+        String createCommentQuery = "UPDATE Comment set content = ?, status = 1 WHERE userId = ? and pageId = ? and blockId = ?";
+        Object[] createCommentParams = new Object[]{commentReq.getContent(),commentReq.getUserId(), pageId, blockId};
+        this.jdbcTemplate.update(createCommentQuery, createCommentParams);
+
+        return new PostCommentRes(commentReq.getUserId(), pageId, blockId, commentReq.getContent(), 1, LocalDateTime.now());
+    }
+
+}
