9f980a0c8f685e7d5ad7c85dfc75cbd4163dfd9f
nnlnuu <mhw0502@konkuk.ac.kr>
2022-08-22 22:56:42 +0900

    Fix: add duplicate blocks _ 일부 작성

--- src/main/java/com/example/demo/src/StampPrint/StampPrintDao.java
+++ src/main/java/com/example/demo/src/StampPrint/StampPrintDao.java
@@ -1,12 +1,16 @@
 package com.example.demo.src.StampPrint;
 
+import com.example.demo.src.Page.model.GetBlocksRes;
 import com.example.demo.src.Page.model.PostPageReq;
+import com.example.demo.src.StampPrint.model.GetChildPageBlocksRes;
+import com.example.demo.src.StampPrint.model.GetParentDataRes;
 import com.example.demo.src.StampPrint.model.PostStampReq;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @Repository
 public class StampPrintDao {
@@ -125,12 +129,100 @@ insert into StampAndPrint (blockId, originalId, followerId, followeeId, newBlock
         this.jdbcTemplate.update(createStampQuery, createNewStampDataParams);
 
 
+        /** 하위 페이지 탐색 및 복제 */
+        duplicateDFS(postStampReq.getBlockId(), newBlockId);
 
         return true;
     }
 
+    int depth=0;
+
+    public void duplicateDFS(int blockId, int newBlockId){
+        String hasChildPageQuery = "select exists(select if(childPageId is null, 0, 1) from Block where blockId=?);";
+        int hasChildPage = this.jdbcTemplate.queryForObject(hasChildPageQuery, int.class, blockId);
+
+        if(hasChildPage == 1){// 하위 페이지가 있을 때
+            // 하위 페이지의 아이디 - 페이지 내 블럭을 탐색할 때 사용 (curPageId 로 검색)
+            String getChildPageIdQuery = "select p.pageId\n" +
+                    "from Block b, Page p\n" +
+                    "where p.parentBlockId = b.blockId and b.blockId=?;";
+            int childPageId = this.jdbcTemplate.queryForObject(getChildPageIdQuery, int.class, blockId);
+
+            // 1. 페이지 복제
+            //    1-1. 페이지 생성
+            //         - parentPageId, parentBlockId, userId, topOrNot(0 고정), preview, stampOrPrint('S' 고정), depth
+            //          새로운 쿼리 - parentPageId, userId, depth / preview
+            //    1-2. 페이지 내 블럭 복제 (원래 페이지의 블럭들)
+            //         - userId, curPageId(새로 생성된 페이지), childPageId, content(가져와야됨), orderNum
+            //          새로운 쿼리 - userId, content
+            // 2. 페이지 내 블럭의 하위 페이지 탐색 및 복제 (재귀 ?)
+
+
+            String getParentDataQuery = "select b.curPageId, b.userId, p.depth\n" +
+                    "from Page p, Block b\n" +
+                    "where p.pageId = b.curPageId and blockId=?;";
+
+            GetParentDataRes getParentDataRes = this.jdbcTemplate.queryForObject(getParentDataQuery,
+                    (rs, rowNum) -> new GetParentDataRes(
+                            rs.getInt("curPageId"),
+                            rs.getInt("userId"),
+                            rs.getInt("depth")
+                    ), newBlockId);
+
+            String getPreviewQuery = "select p.preview\n" +
+                    "from Page p, Block b\n" +
+                    "where p.pageId = b.childPageId and b.blockId=?;";
+
+            String preview = this.jdbcTemplate.queryForObject(getPreviewQuery, String.class, blockId);
+
+            /** 페이지 복제 */
+            String duplicatePageQuery = "insert into Page (parentPageId, parentBlockId, userId, topOrNot, preview, stampOrPrint, depth) values (?, ?, ?, 0, ?, 'S', ?);";
+            Object[] duplicatePageParams = new Object[]{getParentDataRes.getCurPageId(), newBlockId, getParentDataRes.getUserId(), preview, getParentDataRes.getDepth()+1};
+            this.jdbcTemplate.update(duplicatePageQuery, duplicatePageParams);
+
+
+            /**  페이지 내 블럭 탐색 */
+            String getChildBlocksQuery = "select blockId, content, orderNum\n" +
+                    "from Block\n" +
+                    "where status=1 and curPageId=?;";
+
+            List<GetChildPageBlocksRes> childBlocks = this.jdbcTemplate.query(getChildBlocksQuery,
+                    (rs, rowNum) -> new GetChildPageBlocksRes(
+                        rs.getInt("blockId"),
+                        rs.getString("content"),
+                        rs.getInt("orderNum")
+            ), newBlockId);
+
+            // TODO dfs 구현..
+
+
+
+            /**  페이지 내 블럭 복제 */
+
+            String getNewPageIdQuery = "select pageId\n" +
+                    "from Page\n" +
+                    "where parentBlockId = ?;";
+            int newPageId = this.jdbcTemplate.queryForObject(getNewPageIdQuery, int.class, newBlockId);
+
+            String getContentQuery = "select content \n" +
+                    "from Block \n" +
+                    "where blockId = ?;";
+            String content = this.jdbcTemplate.queryForObject(getContentQuery, String.class, blockId);
+
+            String duplicateBlocksQuery = "insert into Block (userId, curPageId, content, orderNum) values (?, ?, ?, ?);";
+            Object[] duplicateBlocksParams = new Object[]{getParentDataRes.getUserId(), newPageId, content, orderNum};
+            this.jdbcTemplate.update(duplicateBlocksQuery, duplicateBlocksParams);
+
+        }
+
+
+        // 하위 페이지가 없을 때, 메소드 종료
+    }
+
+
+
     /*
-     *
+     * stamp 에서 footprint 로 변경할 때
      * */
     public Boolean updateToPrint(int blockId){
         String updateToPrintQuery = "";
